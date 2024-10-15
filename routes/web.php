2e4a309766a56d6dfc9fac51695879d6b79c7a0c
RonaldTrindade <ronaldcruzrct@gmail.com>
2024-10-15 16:50:39 -0300

    0.3

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,5 @@ use App\Http\Controllers\Controller;
 use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-}); 
-
+Route::get('/', function () {return view('welcome');}); 
 Route::get('/login',[UsersController::class,'telaLogin'])->name('views.entrada.login');
